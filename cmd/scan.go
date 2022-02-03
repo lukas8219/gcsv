70b50d0a77452caab0da8f40537a7c56f8e61c45
Lucas Polesello <lucas.c4d@gmail.com>
2022-02-03 01:40:58 -0300

    renamed command to Scan

--- cmd/scan.go
+++ cmd/scan.go
@@ -27,9 +27,9 @@ import (
 	"google.golang.org/api/sheets/v4"
 )
 
-// createCmd represents the create command
-var createCmd = &cobra.Command{
-	Use:   "create",
+// scanCmd represents the scan command
+var scanCmd = &cobra.Command{
+	Use:   "scan",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -37,10 +37,10 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: create,
+	Run: scan,
 }
 
-func create(cmd *cobra.Command, args []string) {
+func scan(cmd *cobra.Command, args []string) {
 	client := getClient()
 	ctx := context.Background()
 
@@ -75,15 +75,15 @@ func create(cmd *cobra.Command, args []string) {
 }
 
 func init() {
-	rootCmd.AddCommand(createCmd)
+	rootCmd.AddCommand(scanCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// createCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// scanCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// scanCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
